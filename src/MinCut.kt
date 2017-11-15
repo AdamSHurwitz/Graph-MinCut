ca840d71d556475235d360c824a27e29a4e7038d
Adam Hurwitz <adamscotthurwitz@gmail.com>
2017-11-14 21:58:10 -0800

    refactor with HashMap

--- src/MinCut.kt
+++ src/MinCut.kt
@@ -1,68 +1,63 @@
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MinCut {
 
     companion object {
         var mergeNode: Int = 1
-        var removeNode: Int = 3
+        var removeNode: Int = 1 //val is 3
 
-        fun minCut(graph: ArrayList<ArrayList<Int>>): Int {
+        fun minCut(graph: HashMap<Int, ArrayList<Int>>) {
+        //fun minCut(graph: HashMap<Int, ArrayList<Int>>): Int {
             println("graph: " + graph.toString())
             mergeNode(graph, mergeNode, removeNode)
-            mergeNode(graph, 2, 4)
+            //mergeNode(graph, 2, 4)
             println("graph: " + graph.toString())
-            return graph[0].size - 1
+            //return graph[0].size - 1
         }
 
         /*
         merge chosen adjacent node into chosen node to merge
         */
-        private fun mergeNode(graph: ArrayList<ArrayList<Int>>, mergeNodeNum: Int, removeNodeNum: Int) {
+        private fun mergeNode(graph: HashMap<Int, ArrayList<Int>>, mergeNodeNum: Int, adjacentNodeNum: Int) {
             //todo: choose random node and adjacent node to removeNode
-            var randomNode = Random().nextInt(graph.size)
+            /*var randomNode = Random().nextInt(graph.size)
             var randomAdjacentNode = Random().nextInt(graph[randomNode].size)
             println("graphSize: " + (graph.size - 1) + " randomGraph: " + randomNode
-                    + " nodeSize: " + (graph[randomNode].size - 1) + " randomAdjacentNode: " + randomAdjacentNode)
+                    + " nodeSize: " + (graph[randomNode].size - 1) + " randomAdjacentNode: " + randomAdjacentNode)*/
             //todo: set while loop to only run when there are more than 2 nodes
 
             //todo: run n - 1 times
             //todo: file input
 
-            var mergeNode = getNode(graph, mergeNodeNum)
-            var removeNode = getNode(graph, removeNodeNum)
-            println("merge: " + mergeNode + " remove: " + removeNode)
+            var mergeNode = graph.getValue(mergeNodeNum)
+            var removeNodeNum = mergeNode[adjacentNodeNum]
 
-            for (i in 1 .. removeNode.second.size - 1) {
-                println("merge: " + removeNode.second[i])
+            println("merge: " + mergeNode + " remove: " + removeNodeNum)
+
+            for (i in 0 .. graph.getValue(removeNodeNum).size - 1) {
+                println("merge: " + graph.getValue(removeNodeNum)[i])
 
                 //add edges from chosen removed node into chosen merged node
-                if (removeNode.second[i] != mergeNode.second[0]) {
-                    mergeNode.second.add(removeNode.second[i])
+                if (graph.getValue(removeNodeNum)[i] != mergeNodeNum) {
+                    mergeNode.add(graph.getValue(removeNodeNum)[i])
                 }
             }
 
             //replace occurence of edges for chosen removed node with chosen merged node
-            for (g in graph){
-                if (g[0] != mergeNodeNum && g.contains(removeNodeNum)) {
-                    g[g.indexOf(removeNodeNum)] = mergeNodeNum
+            for (node in graph){
+                if (node.key != mergeNodeNum && node.value.contains(removeNodeNum)) {
+
+                    node.value[node.value.indexOf(removeNodeNum)] = mergeNodeNum
                 }
             }
 
             //remove occurence of chosen removed node from chosen merged node
-            mergeNode.second.removeAt(mergeNode.second.indexOf(removeNodeNum))
+            mergeNode.removeAt(mergeNode.indexOf(removeNodeNum))
 
             //remove chosen removed node from graph
-            graph.removeAt(removeNode.first)
-        }
-
-        //todo: refactor into HashMap
-        private fun getNode(graph: ArrayList<ArrayList<Int>>, node: Int): Pair<Int, ArrayList<Int>> {
-            for (i in 0 .. graph.size) {
-                if (graph[i][0] == node) {
-                    return Pair(i , graph[i])
-                }
-            }
-            return Pair(0 , arrayListOf())
+            graph.remove(removeNodeNum)
         }
 
     }
--- src/MinCutTest.kt
+++ src/MinCutTest.kt
@@ -1,10 +1,16 @@
 object MinCutTest{
     @JvmStatic
     fun main(args: Array<String>){
-        test(arrayListOf<ArrayList<Int>>(arrayListOf(1, 2, 3), arrayListOf(2, 1, 3, 4), arrayListOf(3, 1, 2, 4), arrayListOf(4, 2, 3)))
+        val hashMap = HashMap<Int, ArrayList<Int>>()
+        hashMap.put(1, arrayListOf(2, 3))
+        hashMap.put(2, arrayListOf(1, 3, 4))
+        hashMap.put(3, arrayListOf(1, 2, 4))
+        hashMap.put(4, arrayListOf(2, 3))
+        test(hashMap)
+        //test(arrayListOf<ArrayList<Int>>(arrayListOf(1, 2, 3), arrayListOf(2, 1, 3, 4), arrayListOf(3, 1, 2, 4), arrayListOf(4, 2, 3)))
     }
 
-    fun test(graph: ArrayList<ArrayList<Int>>){
+    fun test(graph: HashMap<Int, ArrayList<Int>>){
         println("The minimum cut of the graph is " + MinCut.minCut(graph))
     }
 }
